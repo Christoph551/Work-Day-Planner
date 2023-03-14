bac5ce435f00af4f390219f11b0d4e44093332fb
Christoph551 <christophersimmonds551@gmail.com>
2023-03-13 17:35:25 -0700

    Wrote out Javascript functions and for loops to iterate through workday hours and dynamically render timeblocks. Got rid of commented out code. Set up README.md file.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Work-Day-Planner
+
+  [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+## Description 
+
 A simple application to leave notes in timeslots throughout the workday.
+
+## Table of Contents
+
+ - [Mockup](#Mockup)
+ - [Installation](#Installation)
+ - [Deployment](#Deployment)
+ - [Usage](#Usage)
+ - [Questions](#Questions)
+
+## Mockup
+
+![A Screenshot of the deployed site](./Assets/Screenshot.PNG)
+
+## Installation
+
+The Dayjs npm package was used for this application.
+To use this, simply navigate to your working directory in the command line and type `npm install dayjs`.
+
+## Deployment 
+
+This application can be viewed in the browser if you clone this repository, but it is also being hosted on Github Pages: https://Christoph551.github.io/Work-Day-Planner
+
+## Usage 
+
+Leave yourself helpful notes throughout a 9-5 workday! Simply open the application in your browser where you will see a 9-5 timeblock laid out. 
+
+The dayjs npm package tracks the current time and JavaScript has been implemented to keep track of when a note was left. Grey blocks are past, green are future time blocks while red will indicate the current hour.
--- script.js
+++ script.js
@@ -1,28 +1,83 @@
-const dayjs = require('dayjs');
+const $containerFluid = $('.container-fluid')
+const currentDate = dayjs().format('MMMM D YYYY')
+const $currentDate = $('#currentDay')
 
-dayjs().format();
+// Prints Current Date
+$currentDate.append(currentDate)
 
+const currentTime = dayjs().hour()
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// Setting up an array of timeslots for the 9-5 workday
+let workDay = [
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+]
+
+for (i=0; i < workDay.length; i++) {
+  // Define
+  let hourListDiv = $('<div>'); 
+  let hourListChild = $('<div>');
+  let hourListTextArea = $('<textarea>');
+  let hourListSaveBtn = $('<button>');
+  let saveBtnIcon = $('<i>');
+
+  // Classes Text Content Attributes
+  hourListDiv.addClass('row time-block past');
+  hourListTextArea.attr('id', 'hour-'+ workDay[i]);
+  hourListTextArea.addClass('textInput');
+
+  if (workDay[i] == currentTime) {
+    hourListDiv.addClass('row time-block present');
+  } else if (workDay[i] > currentTime ) {
+    hourListDiv.addClass('row time-block future');
+  } else if (workDay[i] < currentTime) {
+    hourListDiv.addClass('row time-block past');
+  }
+
+  hourListChild.addClass('col-2 col-md-1 hour text-center py-3');
+  hourListTextArea.addClass('col-8 col-md-10 description');
+  hourListSaveBtn.addClass('btn saveBtn col-2 col-md-1');
+  saveBtnIcon.addClass('fas fa-save');
+
+  // Converts 24 hour clock to 12 hour
+  if (workDay[i] < 12 ) {
+    hourListChild.text(workDay[i]+"AM")
+  } else if (workDay[i] >= 13) {
+    let convertTime = (workDay[i]-12);
+    hourListChild.text(convertTime+"PM")
+  } else if (workDay[i] >= 12 ) {
+    hourListChild.text(workDay[i]+"PM")
+  } 
+
+  // Where to Display on the page
+  $containerFluid.append(hourListDiv)
+  hourListDiv.append(hourListChild)
+  hourListDiv.append(hourListTextArea)
+  hourListDiv.append(hourListSaveBtn)
+  hourListSaveBtn.append(saveBtnIcon)
+
+}
+
+$(document).ready (function () {
+
+  $('.saveBtn').on("click", function(e){
+    let note = $(this).siblings(".textInput").val();
+    let timeSlot = $(this).siblings(".textInput").attr("id");
+
+    localStorage.setItem(timeSlot, note);
+  });
+  
+
+// Pull note from local storage
+for (i=9; i<=17; i++) {
+  $("#hour-"+i).text(localStorage.getItem("hour-"+i))
+}
 });
+
